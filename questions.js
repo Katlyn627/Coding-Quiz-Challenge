e782d9d3bf5b52c8c64e4eedbd8fd06e1322d470
Katlyn Boches <katlynboches@gmail.com>
2021-09-20 11:12:58 -0600

    Created quiz questions

---
+++ questions.js
@@ -0,0 +1,44 @@
+// Created var questions for quiz
+var questions = [
+    {
+        title: "What does HTML stand for?",
+        choices: ["Hypertext Markup Language", "Hypo Text Mockup Language", "Hyper Text Marketing Language", "Hyper Text Markup Leveler"],
+        answer: "Hypertext Markup Language"
+    },
+    {
+        title: "A loop that never ends is referred to as a(n)_________.",
+        choices: ["Infinite loop", "While loop", "For loop", "Recursive loop"],
+        answer: "Infinite loop"
+    },
+    {
+        title: "In JavaScript, what is a block of code called that is used to perform a specific task?",
+        choices: ["variable", "function", "boolean", "array"],
+        answer: "function"
+    },
+    {
+        title: "_______ is the process of finding errors and fixing them within a program.",
+        choices: ["compiling", "executing", "debugging", "scanning"],
+        answer: "debugging"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+    {
+        title: "Sal needs to execute a section of code ten times within a program. Compare the selection structures below and select which one meets the needs identified.",
+        choices: ["If-Else", "For", "While", "If"],
+        answer: "For"
+    },
+    {
+        title: "Kim has just constructed her first for loop within the Java language.  Which of the following is not a required part of a for loop?",
+        choices: ["Initialization", "Condition", "Variable", "Increment"],
+        answer: "Variable"
+    },
+    {
+        title: "Who created Javascript?",
+        choices: ["Brendan Eich", "Bill Gates", "Tim Berners-Lee", "Dave Shea"],
+        answer: "Brendan Eich"
+    },
+
+];
